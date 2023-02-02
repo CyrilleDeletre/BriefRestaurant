e99542aa7eeba274b89ce5909c9b1f2575a16aef
ncilliez <ncilliez@gmail.com>
2023-02-02 14:48:09 +0100

    debut composant entrée

--- src/components/Pages/Plats/Plats.js
+++ src/components/Pages/Plats/Plats.js
@@ -1,10 +1,29 @@
 import Menu from '../../Elements/Menu/Menu';
+import Data from '../../Data/Datacarte';
 
 const Plats = () => {
     return (
         <>
             <Menu />
-            <p>Je suis la page plats</p>
+            <main>
+                <h2>Je suis la page plats</h2>
+                <ul>
+                    {Data.map((uneData, i) =>
+                        {
+                            if (uneData.type === 'entree') 
+                            {
+                                return (
+                                    <li key={i}>
+                                        <img src={`img/${uneData.illustration}`} alt="un txt au pif"/>
+                                        <p>{uneData.libelle}</p>
+                                        <p>{uneData.tarif}</p>
+                                    </li>
+                                )
+                            }
+                        }
+                    )}
+                </ul>
+            </main>
         </>
     )
 }
