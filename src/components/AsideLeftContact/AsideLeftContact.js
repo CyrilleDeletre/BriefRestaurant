cdd82ad039709928942d23351e943538dd32eec9
CyrilleDeletre <cyrille.deletre@gmail.com>
2023-02-02 13:35:07 +0100

    push table

--- src/components/AsideLeftContact/AsideLeftContact.js
+++ src/components/AsideLeftContact/AsideLeftContact.js
@@ -3,7 +3,57 @@ import '../../components/AsideLeftContact/AsideLeftContact.css';
 function AsideLeftContact() {
   return (
     <aside>
-        <div>Blabla</div>
+        <section>
+          <h2>Adresse du restaurant</h2>
+          <p>297, chaussée Fernand Forest , Tourcoing, France</p>
+        </section>
+        <section>
+          <h2>Numéro de téléphone du restaurant</h2>
+          <p>03 20 23 84 65</p>
+        </section>
+        <section>
+          <h2>Horaire d'ouverture</h2>
+          <table>
+            <tr>
+              <td>Lundi</td>
+              <td>12:00 - 14:30</td>
+            </tr>
+
+            <tr>
+              <td>Mardi</td>
+              <td>12:00 - 14:30</td>
+            </tr>
+
+            <tr>
+              <td>Mercredi</td>
+              <td>12:00 - 14:30</td>
+            </tr>
+
+            <tr>
+              <td>Jeudi</td>
+              <td>12:00 - 14:30</td>
+            </tr>
+
+            <tr>
+              <td>Vendredi</td>
+              <td>12:00 - 14:30</td>
+              <td>18:00 - 23:00</td>
+            </tr>
+
+            <tr>
+              <td>Samedi</td>
+              <td>12:00 - 14:30</td>
+              <td>18:00 - 23:00</td>
+            </tr>
+
+
+            <tr>
+              <td>Dimanche</td>
+              <td>FERMÉ</td>
+            </tr>
+            
+          </table>
+        </section>
     </aside>
   );
 }
