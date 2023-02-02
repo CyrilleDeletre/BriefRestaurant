fe5fa6cf8a07f9595385ed86ece94ed98fb051f8
ncilliez <ncilliez@gmail.com>
2023-02-02 12:23:36 +0100

    photos et jeu de donnée

---
+++ src/components/Data/Datacarte.js
@@ -0,0 +1,120 @@
+const carte=[
+    {
+        type: 'entree',
+        tarif: '6.00',
+        libelle: 'Darioles gambas, jus de crustacés',
+        illustration: 'darioles.jpg'
+    },
+    {
+        type: 'entree',
+        tarif: '6.00',
+        libelle: 'Gougères d\'escargots à l\'ail',
+        illustration: 'gougeres.jpg'
+    },
+    {
+        type: 'entree',
+        tarif: '6.00',
+        libelle: 'Oeufs à la Florentine',
+        illustration: 'oeufs.jpg'
+    },
+    {
+        type: 'plat',
+        tarif: '17.90',
+        libelle: 'Pièce de Boeuf grillée, frites, salade',
+        illustration: 'boeuf.jpg'
+    },
+    {
+        type: 'plat',
+        tarif: '17.90',
+        libelle: 'Tartare de boeuf préparé, frites et salade',
+        illustration: 'tartare.jpg'
+    },
+    {
+        type: 'plat',
+        tarif: '17.90',
+        libelle: 'Carbonnade Flamande à la bière de saison, frites et salade',
+        illustration: 'carbonnade.jpg'
+    },
+    {
+        type: 'plat',
+        tarif: '17.90',
+        libelle: 'Welsh Royal au jambon fumé, frites et salade',
+        illustration: 'welsh.jpg'
+    },
+    {
+        type: 'plat',
+        tarif: '17.90',
+        libelle: 'Crumble de poissons sur lit de poireaux et pomme écrasée',
+        illustration: 'crumble.jpg'
+    },
+    {
+        type: 'dessert',
+        tarif: '6.00',
+        libelle: 'Millefeuille de poire caramélisée et choco',
+        illustration: 'millefeuille.jpg'
+    },
+    {
+        type: 'dessert',
+        tarif: '6.00',
+        libelle: 'Merveilleux déstructuré au chocolat blanc',
+        illustration: 'merveilleux.jpg'
+    },
+    {
+        type: 'dessert',
+        tarif: '6.00',
+        libelle: 'Coupe de Glace 2 boules (Parfums: Vanille, Chocolat, Citron, Fraise',
+        illustration: 'crumble.jpg'
+    },
+    {
+        type: 'alcoolise',
+        tarif: '8.00',
+        libelle: 'Vin Sauvignon',
+        illustration: 'sauvignon.jpg'
+    },
+    {
+        type: 'alcoolise',
+        tarif: '25.00',
+        libelle: 'Vin Sancerre',
+        illustration: 'sancerre.jpg'
+    },
+    {
+        type: 'alcoolise',
+        tarif: '5.00',
+        libelle: 'Tripel Karmeliet',
+        illustration: 'karmeliet.jpg'
+    },
+    {
+        type: 'soft',
+        tarif: '2.00',
+        libelle: 'Perrier',
+        illustration: 'perrier.jpg'
+    },
+    {
+        type: 'soft',
+        tarif: '2.00',
+        libelle: 'Jus d\'orange',
+        illustration: 'orange.jpg'
+    },
+    {
+        type: 'soft',
+        tarif: '2.00',
+        libelle: 'Limonade',
+        illustration: 'limonade.jpg'
+    },
+    {
+        type: 'chaude',
+        tarif: '6.00',
+        libelle: 'Café',
+        illustration: 'cafe.jpg'
+    },
+    {
+        type: 'chaude',
+        tarif: '6.00',
+        libelle: 'Thé citron',
+        illustration: 'thecitron.jpg'
+    }
+
+];
+
+export default carte;
+
