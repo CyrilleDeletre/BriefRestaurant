543c326198cb352267f7d7da5e1ab457c918f81a
CyrilleDeletre <cyrille.deletre@gmail.com>
2023-02-02 13:50:29 +0100

    ajout slider

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,13 +1,15 @@
 import Menu from '../../Elements/Menu/Menu';
+import Slider from '../../Elements/Slider/Slider';
 import Presentation from '../../Presentation/Presentation';
 import Footer from '../../Elements/Footer/Footer';
 import '../Home/Home.css';
+
 
 const Home = () => {
     return (
         <>
             <Menu />
-            <p>Slider</p>
+            <Slider />
             <h1>Accueil</h1>
             <Presentation />
             <Footer />
