1f036b8816c55a56b9e4f70eb8a8192ca72fb45b
ncilliez <ncilliez@gmail.com>
2023-02-03 15:50:57 +0100

    slider

--- src/components/Elements/Slider/Slider.js
+++ src/components/Elements/Slider/Slider.js
@@ -1,45 +1,51 @@
-import { logDOM } from "@testing-library/react";
 import React, { useEffect } from "react";
 import {useState} from "react";
 import Data from '../../Data/DataSlider';
 import './Slider.css';
 
-
 const Slider = () => {
+
     const [slide, setSlide] = useState(0);
 
     const target = (variable, slide)=>{
+       
         variable = slide;
         setSlide(variable);
     }
 
     const play = () =>{
-        // clearInterval(play);
-            setInterval(() => {
-                if(slide!== Data.length-1){
-                    setSlide(slide+1);
-                }
-                else{
-                    setSlide(0);
-                } 
-            }, "4000");}
-              
-        useEffect(() => {
-            play(); 
-    })
+        
+        setInterval(() => {
+
+            if(slide !== Data.length-1){
+                setSlide(slide + 1);
+            }
+            else{
+                setSlide(0);
+            } 
+
+        }, "4000"); 
+
+    } 
+
+    useEffect(() => {      
+        play();   
+    });
 
     return (
+
         <div className="slider">
         <div><img src={Data[slide].url} alt={Data[slide].alt}/></div>
 
-        <ul className="puces">
-        {Data.map((uneImage, i) => (
-            <li key={uneImage.id} onClick = {() => target(slide, i)} className={i === slide ? 'target' : 'puce'}>
-            </li>
-       ))}   
-        </ul>
+        {/* <ul className="puces">
+            {Data.map((uneImage, i) => (
+                <li key={uneImage.id} onClick = {() => target(slide, i)} className={i === slide ? 'target' : 'puce'}>
+                </li>
+            ))}   
+        </ul> */}
 
         </div>
     )
+    
 }
 export default Slider;
