26bc7614e2b2a27680a06164b517be09cd2e5193
ncilliez <ncilliez@gmail.com>
2023-02-06 10:49:15 +0100

    sliter et liste a puces

--- src/components/Elements/Slider/Slider.js
+++ src/components/Elements/Slider/Slider.js
@@ -2,40 +2,35 @@ import React, { useEffect } from "react";
 import {useState} from "react";
 import Data from '../../Data/DataSlider';
 import './Slider.css';
-// var b = false;
 
 const Slider = () => {
 
     const [slide, setSlide] = useState(0);
 
-    // const target = (variable, slide)=>{
-    //     b = true;
-    //     variable = slide;
-    //     setSlide(variable);
-    // }
+    const target = (variable, slide)=>{
+        variable = slide;
+        setSlide(variable);
+    }
 
-    const play = () =>{
+    // const play = () =>{
         
-        setInterval(() => {
+      
 
+    // } 
+
+    useEffect(() => {  
+        const timer = setInterval(() => {
+            
             if(slide !== Data.length-1){
                 setSlide(slide + 1);
             }
             else{
+                
                 setSlide(0);
             } 
 
         }, "4000"); 
-
-    } 
-
-    useEffect(() => {  
-        // if(b === true){
-           
-        // }else{
-            play();   
-        // }
-       
+       return () => clearInterval(timer);
     });
 
     return (
@@ -43,12 +38,12 @@ const Slider = () => {
         <div className="slider">
         <div><img src={Data[slide].url} alt={Data[slide].alt}/></div>
 
-        {/* <ul className="puces">
+        <ul className="puces">
             {Data.map((uneImage, i) => (
                 <li key={uneImage.id} onClick = {() => target(slide, i)} className={i === slide ? 'target' : 'puce'}>
                 </li>
             ))}   
-        </ul> */}
+        </ul>
 
         </div>
     )
